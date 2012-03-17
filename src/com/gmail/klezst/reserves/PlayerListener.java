b5dc5d775758a4e44361f3df6e97568221aa98dd
Klezst <Klezst@gmail.com>
2012-03-17 13:56:04 -0400

    Updated to use BukkitUtil.

--- src/com/gmail/klezst/reserves/PlayerListener.java
+++ src/com/gmail/klezst/reserves/PlayerListener.java
@@ -9,6 +9,8 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
+import bukkitutil.compatibility.Permission;
+
 /**
  * Determines, if a player needs to be kicked.
  * 
@@ -18,7 +20,7 @@ public class PlayerListener implements Listener {
     private final String permissionNode = "reserves";
 
     private LinkedList<Player> playerJoinOrder = new LinkedList<Player>();
-
+    
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
 	// Used to determine which player has been on the longest.
@@ -35,11 +37,11 @@ public class PlayerListener implements Listener {
     public void onPlayerPreLogin(PlayerLoginEvent event) {
 	// Determine, if the server is full && if the player has a reserved slot.
 	if (event.getResult() == PlayerLoginEvent.Result.KICK_FULL
-		&& Reserves.permission.has(event.getPlayer(), permissionNode)) {
+		&& Permission.has(event.getPlayer(), permissionNode)) {
 
 	    // Kick the player who has been connected longest and does not have a slot reserved.
 	    for (Player player : playerJoinOrder) {
-		if (!Reserves.permission.has(player, permissionNode)) {
+		if (!Permission.has(player, permissionNode)) {
 		    player.kickPlayer("Sorry, the server is full; a player with a reserved slot logged in.");
 		    event.allow(); // Allow the connecting player to join the server despite that it was full.
 		    break;
--- src/com/gmail/klezst/reserves/Reserves.java
+++ src/com/gmail/klezst/reserves/Reserves.java
@@ -1,27 +1,24 @@
 package com.gmail.klezst.reserves;
 
 import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import net.milkbowl.vault.permission.Permission;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.plugin.RegisteredServiceProvider;
-import org.bukkit.plugin.java.JavaPlugin;
+
+import bukkitutil.BukkitUtilJavaPlugin;
 
 /**
  * Enables the reservation of player slots, in case the server is at max players.
  * 
  * @author Klezst
  */
-public class Reserves extends JavaPlugin {
-
-    private static Logger logger = Logger.getLogger("Minecraft");
-    protected static Permission permission = null;
-
+public class Reserves extends BukkitUtilJavaPlugin {
     private PlayerListener listener = new PlayerListener();
-
+    
+    public Reserves() {
+	super("[Reserves]");
+    }
+    
     @Override
     public boolean onCommand(CommandSender sender, Command cmd,
 	    String commandLabel, String[] args) {
@@ -35,37 +32,7 @@ public class Reserves extends JavaPlugin {
 
     @Override
     public void onEnable() {
-	setupPermissions();
 	this.getServer().getPluginManager().registerEvents(listener, this);
 	log(Level.INFO, "Enabled.");
-    }
-
-    /**
-     * Logs a message.
-     * 
-     * @param level
-     *            The importance of the message.
-     * @param message
-     *            The message to log.
-     * 
-     * @author Klezst
-     */
-    private void log(Level level, String message) {
-	logger.log(level, "[Reserves]" + message);
-    }
-
-    /**
-     * Loads the Vault permission management system.
-     * 
-     * @author Vault
-     */
-    private Boolean setupPermissions() {
-	RegisteredServiceProvider<Permission> permissionProvider = getServer()
-		.getServicesManager().getRegistration(
-			net.milkbowl.vault.permission.Permission.class);
-	if (permissionProvider != null) {
-	    permission = permissionProvider.getProvider();
-	}
-	return (permission != null);
     }
 }
